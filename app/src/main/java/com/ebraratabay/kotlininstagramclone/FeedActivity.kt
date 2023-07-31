2094f929a0949341a6e50113836b8fd8f8a139ed
MaryTennyson <ebrar-atabay@hotmail.com>
2023-07-31 18:13:11 +0300

    Firebase Auth added

--- app/src/main/java/com/ebraratabay/kotlininstagramclone/FeedActivity.kt
+++ app/src/main/java/com/ebraratabay/kotlininstagramclone/FeedActivity.kt
@@ -2,10 +2,36 @@ package com.ebraratabay.kotlininstagramclone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ContextMenu
+import android.view.LayoutInflater
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
+import com.ebraratabay.kotlininstagramclone.databinding.ActivityFeedBinding
 
 class FeedActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityFeedBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_feed)
+        binding= ActivityFeedBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        registerForContextMenu(binding.popUpMenu)
+    }
+
+    override fun onCreateContextMenu(
+        menu: ContextMenu?,
+        v: View?,
+        menuInfo: ContextMenu.ContextMenuInfo?
+    ) {
+        super.onCreateContextMenu(menu, v, menuInfo)
+        menuInflater.inflate(R.menu.insta_menu,menu)
+    }
+
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.addpost-> Toast.makeText(this, "Add post",Toast.LENGTH_LONG)
+            R.id.signout-> Toast.makeText(this, "Sign Out",Toast.LENGTH_LONG)
+        }
+        return super.onContextItemSelected(item)
     }
 }
--- app/src/main/java/com/ebraratabay/kotlininstagramclone/UploadActivity.kt
+++ app/src/main/java/com/ebraratabay/kotlininstagramclone/UploadActivity.kt
@@ -2,10 +2,19 @@ package com.ebraratabay.kotlininstagramclone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class UploadActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_upload)
+    }
+
+    fun onClickAddImage(view : View){
+
+
+    }
+    fun onClickUploadPost(view:View){
+
     }
 }
