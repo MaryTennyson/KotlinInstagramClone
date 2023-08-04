c285e7f19754f33bc90a94a6fcaac6d6cb3db5be
MaryTennyson <ebrar-atabay@hotmail.com>
2023-08-04 21:09:36 +0300

    Popup menu created

--- app/src/main/java/com/ebraratabay/kotlininstagramclone/FeedActivity.kt
+++ app/src/main/java/com/ebraratabay/kotlininstagramclone/FeedActivity.kt
@@ -1,37 +1,57 @@
 package com.ebraratabay.kotlininstagramclone
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.ContextMenu
 import android.view.LayoutInflater
+import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
+import android.widget.PopupMenu
 import android.widget.Toast
 import com.ebraratabay.kotlininstagramclone.databinding.ActivityFeedBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class FeedActivity : AppCompatActivity() {
     private lateinit var binding : ActivityFeedBinding
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding= ActivityFeedBinding.inflate(layoutInflater)
         setContentView(binding.root)
         registerForContextMenu(binding.popUpMenu)
+        auth=Firebase.auth
     }
 
-    override fun onCreateContextMenu(
-        menu: ContextMenu?,
-        v: View?,
-        menuInfo: ContextMenu.ContextMenuInfo?
-    ) {
-        super.onCreateContextMenu(menu, v, menuInfo)
-        menuInflater.inflate(R.menu.insta_menu,menu)
-    }
 
-    override fun onContextItemSelected(item: MenuItem): Boolean {
-        when(item.itemId){
-            R.id.addpost-> Toast.makeText(this, "Add post",Toast.LENGTH_LONG)
-            R.id.signout-> Toast.makeText(this, "Sign Out",Toast.LENGTH_LONG)
+    fun showPopUponExtra(view: View){
+        val popup= PopupMenu(this,view)
+        val inflater: MenuInflater= popup.menuInflater
+        inflater.inflate(R.menu.insta_menu, popup.menu)
+        popup.setOnMenuItemClickListener { menuItem ->
+            when(menuItem.itemId){
+                R.id.signout->{
+                    auth.signOut()
+                    val intent= Intent(this@FeedActivity,MainActivity::class.java)
+                    startActivity(intent)
+                    finish()
+
+                }
+                R.id.addpost->{
+                    val intent= Intent(this@FeedActivity,UploadActivity::class.java)
+                    startActivity(intent)
+                    finish()
+
+                }
+            }
+            true
         }
-        return super.onContextItemSelected(item)
+        popup.show()
+
     }
+
+
 }
--- app/src/main/java/com/ebraratabay/kotlininstagramclone/UploadActivity.kt
+++ app/src/main/java/com/ebraratabay/kotlininstagramclone/UploadActivity.kt
@@ -1,8 +1,12 @@
 package com.ebraratabay.kotlininstagramclone
 
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.material.snackbar.Snackbar
 
 class UploadActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,6 +15,14 @@ class UploadActivity : AppCompatActivity() {
     }
 
     fun onClickAddImage(view : View){
+        if(ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED){
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this,android.Manifest.permission.READ_EXTERNAL_STORAGE)){
+                Snackbar.make(view,"Permission needed for Gallery", Snackbar.LENGTH_INDEFINITE).setAction("Give Permission"){
+                    //request permission
+
+                }.show()
+            }
+        }
 
 
     }
